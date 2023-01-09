2dd173618f2a609c3c0ed83dc1e04a226277d8ee
QuangND <quangnd.dev@gmail.com>
2023-01-09 08:22:23 +0700

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ExUseState from './components/ExUseState';
+
+function App() {
+  return (
+    <div className="App">
+      <ExUseState></ExUseState>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExUseState.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+export default function ExUseState() {
+    // Khởi tạo giá trị state với hàm useState
+    cost[count, setCount] = useState(0);
+    return (
+        <div>
+            <h2>Rikkei Academy - React Hooks - useState</h2>
+            <p>Current Value: {count}</p>
+            <button>Up</button>
+            <button>Down</button>
+        </div>
+    )
+}
